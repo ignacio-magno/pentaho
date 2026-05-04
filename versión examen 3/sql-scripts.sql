0ab17ce4cc2b552ccc06512891e29570985d9fc0
ignacio-magno <88296440+ignacio-magno@users.noreply.github.com>
2026-05-03 22:33:03 -0400

    Add new tables for dim_fecha and dim_producto, and update fact_ventas schema

--- versión examen 3/sql-scripts.sql
+++ versión examen 3/sql-scripts.sql
@@ -41,3 +41,34 @@ INSERT INTO inacap_2.categorias (nombre_categoria) VALUES
                                                        ('Accesorios'),
                                                        ('Oficina'),
                                                        ('Viajes');
+
+
+create table inacap_2.dim_fecha
+(
+    id_fecha bigint unsigned auto_increment
+        primary key,
+    dia      int null,
+    mes      int null,
+    año      int null,
+    constraint fecha_unica
+        unique (dia, mes, año),
+    constraint id_fecha
+        unique (id_fecha)
+);
+
+create table inacap_2.dim_producto
+(
+    id_producto     varchar(50) not null
+        primary key,
+    nombre_producto varchar(50) null
+);
+
+create table inacap_2.fact_ventas
+(
+    `Venta/ID`              int         null,
+    `Precio total de venta` bigint      null,
+    monto_total             double      null,
+    id_producto             varchar(50) null,
+    id_fecha                bigint      null
+);
+
